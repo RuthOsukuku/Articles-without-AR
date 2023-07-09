06a34d18a844a264ead6f26a907113ce413932fd
Ruth <osukukuruth@gmail.com>
2023-07-09 19:01:57 -0400

    Done Assignment

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,11 +1,85 @@
-class Author
-  attr_accessor :name
+# class Author
+#   attr_accessor :name
 
+
+#   def initialize(name)
+#     @name = name
+  
+#   end
+
+
+# end
+class Author
+  attr_reader :name
 
   def initialize(name)
     @name = name
-  
   end
 
+  def articles
+    Article.all.select { |article| article.author == self }
+  end
 
+  def magazines
+    articles.map { |article| article.magazine }.uniq
+  end
+
+  def add_article(magazine, title)
+    Article.new(self, magazine, title)
+  end
+
+  def topic_areas
+    magazines.map { |magazine| magazine.category }.uniq
+  end
+end
+
+class Magazine
+  attr_accessor :name, :category
+
+  @@all = []
+
+  def initialize(name, category)
+    @name = name
+    @category = category
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def contributors
+    Article.all.select { |article| article.magazine == self }.map { |article| article.author }
+  end
+
+  def self.find_by_name(name)
+    all.find { |magazine| magazine.name == name }
+  end
+
+  def article_titles
+    Article.all.select { |article| article.magazine == self }.map { |article| article.title }
+  end
+
+  def contributing_authors
+    author_counts = Hash.new(0)
+    Article.all.each { |article| author_counts[article.author] += 1 }
+    author_counts.select { |author, count| count > 2 }.keys
+  end
+end
+
+class Article
+  attr_reader :author, :magazine, :title
+
+  @@all = []
+
+  def initialize(author, magazine, title)
+    @author = author
+    @magazine = magazine
+    @title = title
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,12 +1,46 @@
+# class Magazine
+#   attr_accessor :name, :category
+
+
+#   def initialize(name, category)
+#     @name = name
+#     @category = category
+
+#   end
+
+
+# end
+
 class Magazine
   attr_accessor :name, :category
 
+  @@all = []
 
   def initialize(name, category)
     @name = name
     @category = category
-
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
 
+  def contributors
+    Article.all.select { |article| article.magazine == self }.map { |article| article.author }
+  end
+
+  def self.find_by_name(name)
+    all.find { |magazine| magazine.name == name }
+  end
+
+  def article_titles
+    Article.all.select { |article| article.magazine == self }.map { |article| article.title }
+  end
+
+  def contributing_authors
+    author_counts = Hash.new(0)
+    Article.all.each { |article| author_counts[article.author] += 1 }
+    author_counts.select { |author, count| count > 2 }.keys
+  end
 end
